eec8d4ce99bc795b19c9ad7a976a508519717b48
Kitonsa Elvis <elvis.kitonsa@students.mak.ac.ug>
2025-12-23 22:13:55 +0300

    Feat: Implement instant live search with balanced UI and vector icons

--- shop/views.py
+++ shop/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
+from .models import Product
+from django.db.models import Q # This allows searching multiple fields
 
 # Create your views here.
-from .models import Product
-
 def product_list(request):
-    products = Product.objects.all()
-    return render(request, 'shop/product_list.html', {'products': products})
+    query = request.GET.get('q') # Get what the user typed in the search box
+    
+    if query:
+        # Filter products by name OR description
+        products = Product.objects.filter(
+            Q(name__icontains=query) | Q(description__icontains=query)
+        )
+    else:
+        products = Product.objects.all()
+        
+    return render(request, 'shop/product_list.html', {'products': products, 'query': query})
